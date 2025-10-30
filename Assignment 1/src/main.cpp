fa5771311a11ea23f8ec34093699b4799d4c1d7b
Josh B <89416369+joshb88@users.noreply.github.com>
2025-10-30 00:55:03 -0500

    idk workign on it

--- Assignment 1/src/main.cpp
+++ Assignment 1/src/main.cpp
@@ -5,24 +5,45 @@
 #include <string>
 #include <memory>
 #include <vector>
-
+#include <filesystem>
 #include <SFML/Graphics.hpp>
 #include "imgui.h"
 #include "imgui-SFML.h"
 
 int main(int argc, char* argv[]) {
 
-    std::ifstream config_file("./Assignment 1/bin/config.txt");
+    unsigned int height, width;
+
+    std::ifstream config_file("./bin/config.txt");
 
     if (!config_file) {
-        std::cerr << "Coudn't open the config file." << std::endl;
+        std::cerr << "Couldn't open the config file." << std::endl;
         return -1;
     }
 
-    std::string line;
-    unsigned int width, height;
-    bool windowSizeFound = false;
+    struct shapeData
+    {
+        std::string name;
+        float speed[2]{0.0, 0.0};
+        bool visible = true;
+        float scale = 1.0f;
+        unsigned int vertices = 32;
+
+        std::unique_ptr<sf::Shape> shape;
+        std::unique_ptr<sf::Text> text;
+    };
     
+    bool windowSizeFound = false;
+    bool fontFound = false;
+    sf::Font myFont;
+    unsigned int font_size;
+    sf::Color font_color;
+
+    std::vector<shapeData> shape_list;
+    
+    std::string line;
+    
+    // check for the window/font things first.
     while(std::getline(config_file, line)) {
         std::istringstream iss(line);
         std::string key;
@@ -30,23 +51,74 @@ int main(int argc, char* argv[]) {
         iss >> key;
 
         if (key == "Window") {
+            // Window config
             iss >> width >> height;
             windowSizeFound = true;
+        }
+
+        if (key == "Font") {
+            // File Config
+            std::string font_path_str;
+            
+            iss >> font_path_str >> font_size >> font_color.r >> font_color.g >> font_color.b;
+            std::filesystem::path font_path = std::filesystem::path("./bin/") / font_path_str;
+            if (!myFont.openFromFile(font_path)) {
+                std::cerr << "Couldn't open the font: " << font_path << std::endl;
+                return -1;
+            }
+            fontFound = true;
         }
     }
     
     if (!windowSizeFound) {
         std::cout << "No config settings for Window Size; using default (800x600)" << std::endl;
-        width = 800;
-        height = 600;
+        unsigned int width = 800;
+        unsigned int height = 600;
     }
     const unsigned int wWidth = width, wHeight = height;
+    
+    if (!fontFound) {
+        std::cerr << "No font file found, exiting..." << std::endl;
+        return -1;
+    }
 
-    // create a new window of size w*h pixels
-    // top left of the window is (0,0) and bottom right is (w, h)
-    // you will have to read these from the config file
-    // const int wWidth = 1280;
-    // const int wHeight = 720;
+    // clear and look for shapes
+    config_file.clear();
+    config_file.seekg(0);
+
+    while(std::getline(config_file, line)) {
+        std::istringstream iss(line);
+        std::string key;
+
+        iss >> key;
+
+        if (key == "Rectange") {
+            shapeData rect;
+            float x, y;
+            uint8_t r, g, b;
+            float size[2];
+            iss >> rect.name >> x >> y >> rect.speed[0] >> rect.speed[1] >> r >> g >> b >> size[0] >> size[1];
+            rect.shape = std::make_unique<sf::RectangleShape>(sf::Vector2f(size[0], size[1]));
+            rect.shape->setPosition({x,y});
+            rect.shape->setFillColor({r, g, b});
+            rect.text = std::make_unique<sf::Text>(myFont, rect.name, font_size);
+            shape_list.push_back(std::move(rect));
+        }
+
+        if (key == "Circle") {
+            shapeData circle;
+            float x, y;
+            uint8_t r, g, b;
+            float rad;
+            iss >> circle.name >> x >> y >> circle.speed[0] >> circle.speed[1] >> r >> g >> b >> rad;
+            circle.shape = std::make_unique<sf::CircleShape>(rad, circle.vertices);
+            circle.shape->setPosition({x,y});
+            circle.shape->setFillColor({r, g, b});
+            circle.text = std::make_unique<sf::Text>(myFont, circle.name, font_size);
+            shape_list.push_back(std::move(circle));
+        }
+    }
+    
     sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "SFML works!");
     window.setFramerateLimit(60); // limit frame rate to 60 fps
 
@@ -80,10 +152,10 @@ int main(int argc, char* argv[]) {
     circle.setPosition({ 10.0f, 10.0f });                       // set the top-left position
 
     // let's load a font so we can display text
-    sf::Font myFont;
+    // sf::Font myFont;
 
     // attempt to load the font from a file
-    if (!myFont.openFromFile("./Assignment 1/bin/fonts/PressStart2P.ttf")) {
+    if (!myFont.openFromFile("./bin/fonts/PressStart2P.ttf")) {
         // if we can't load the font, print and error and exit
         std::cerr << "Could not load the font" << std::endl;
         return -1;
@@ -126,11 +198,48 @@ int main(int argc, char* argv[]) {
         ImGui::SFML::Update(window, deltaClock.restart());
 
         // draw the UI
-        ImGui::Begin("Window title");
-        ImGui::Text("WIndow text!");
-        ImGui::Checkbox("Draw Circle", &drawCircle);
-        ImGui::SliderFloat("Radius", &circleRadius, 0.0f, 300.0f);
-        ImGui::SliderInt("Sides", &circleSegments, 3, 64);
+        ImGui::Begin("Shapes");
+        ImGui::Text("Shape Settings");
+
+        std::vector<const char*> shapes_available;
+        for (const auto& shape : shape_list) {
+            shapes_available.push_back(shape.name.c_str());
+        }
+
+        static int item_selected_idx = 0; // Here we store our selection data as an index.
+        const char* combo_preview_value = (item_selected_idx >= 0 && item_selected_idx < shapes_available.size()) ? shapes_available[item_selected_idx] : "Select a shape";
+        if (ImGui::BeginCombo("Shape Selection", combo_preview_value, ImGuiComboFlags_WidthFitPreview))
+        {
+            static ImGuiTextFilter filter;
+            if (ImGui::IsWindowAppearing())
+            {
+                ImGui::SetKeyboardFocusHere();
+                filter.Clear();
+            }
+            ImGui::SetNextItemShortcut(ImGuiMod_Ctrl | ImGuiKey_F);
+            filter.Draw("##Filter", -FLT_MIN);
+
+            for (int n = 0; n < shapes_available.size(); n++)
+            {
+                const bool is_selected = (item_selected_idx == n);
+                if (filter.PassFilter(shapes_available[n]))
+                    if (ImGui::Selectable(shapes_available[n], is_selected))
+                        item_selected_idx = n;
+            }
+            ImGui::EndCombo();
+        }
+
+        shapeData& shape_selected = shape_list[item_selected_idx];
+        // if its a circle, do circle stuff
+        if (auto* circle = dynamic_cast<sf::CircleShape*>(shape_selected.shape.get())) {
+            float radius = circle->getRadius();
+            ImGui::SliderFloat("Radius", &radius, 0.0f, 300.0f);
+            circle->setRadius(radius);
+
+            ImGui::SliderInt("Sides", (int*)&shape_selected.vertices, 3, 64);
+            circle->setPointCount(shape_selected.vertices);
+        }
+        ImGui::Checkbox("Draw Circle", &shape_selected.visible);
         ImGui::ColorEdit3("Color Circle", c);
         ImGui::InputText("Text", displayString, 255);
         if (ImGui::Button("Set Texts")) {
@@ -158,7 +267,7 @@ int main(int argc, char* argv[]) {
         if (drawCircle) {       // draw the circle if the boolean is true
             window.draw(circle);
         }
-        if (drawText) {         // draw the texdt if boolean is true
+        if (drawText) {         // draw the text if boolean is true
             window.draw(text);
         }
         ImGui::SFML::Render(window);    //draw the ui last so its on top
