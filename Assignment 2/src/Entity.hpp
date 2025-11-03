215df61db62c1c03f7e3d6755715fe14d5090dce
Josh B <89416369+joshb88@users.noreply.github.com>
2025-11-03 01:17:10 -0600

    assignment 2

---
+++ Assignment 2/src/Entity.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "Components.hpp"
+#include <string>
+#include <tuple>
+
+class EntityManager;
+
+using ComponentTuple = std::tuple<
+    CTransform,
+    CShape,
+    CCollision,
+    CInput,
+    CScore,
+    CLifespan
+>;
+
+class Entity
+{
+    friend class EntityManager;
+
+    ComponentTuple m_components;
+    bool m_alive = true;
+    const std::string m_tag = "default";
+    size_t m_id = 0;
+
+    Entity(size_t id, const std::string& tag)
+        : m_id(id)
+        , m_tag(tag)
+    {}
+
+public:
+    bool isAlive() const
+    {
+        return m_alive;
+    }
+
+    void destroy()
+    {
+        m_alive = false;
+    }
+
+    size_t id() const
+    {
+        return m_id;
+    }
+
+    const std::string& tag() const
+    {
+        return m_tag;
+    }
+
+    template <typename T>
+    bool has() const
+    {
+        return get<T>().exists;
+    }
+
+    template<typename T, typename... TArgs>
+    void add(TArgs&&... args)
+    {
+        auto& component = get<T>();
+        component = T(std::forward<TArgs>(args)...);
+        component.exists = true;
+    }
+
+    template<typename T>
+    T& get()
+    {
+        return std::get<T>(m_components);
+    }
+
+    template<typename T>
+    const T& get() const
+    {
+        return std::get<T>(m_components);
+    }
+
+
+    template<typename T>
+    void remove()
+    {
+        get<T>() = T();
+    }
+};
+
+
+
---
+++ Assignment 2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+
+#include "Game.h"
+
+int main()
+{
+    // test Vec2
+
+    Game g("config.txt");
+    g.run();
+}
